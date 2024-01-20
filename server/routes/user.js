b38ae4e7acdc6e7326e6071f5e2f81b4e83a30b4
Vanshul Agarwal <vanshulagarwal2004@gmail.com>
2024-01-21 01:36:12 +0530

    search, following,follower

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { login, register, logout, changePassword, setUsername } = require('../controllers/user.js');
+const { login, register, logout, changePassword, setUsername, userDetails, sendFollowRequest, acceptFollowRequest, rejectFollowRequest } = require('../controllers/user.js');
 const { isLoggedIn } = require('../utils/isLoggedIn.js');
 const catchAsync = require('../utils/catchAsync');
 
@@ -12,5 +12,9 @@ router.route('/changepw')
     // .get(changePassword)//changepasswordform
     .post(catchAsync(isLoggedIn), catchAsync(changePassword));
 router.route('/setusername').post(catchAsync(isLoggedIn), catchAsync(setUsername));
+router.route('/userdetails').get(catchAsync(isLoggedIn), catchAsync(userDetails));
+router.route('/sendfrequest').post(catchAsync(isLoggedIn), catchAsync(sendFollowRequest));
+router.route('/acceptfrequest').post(catchAsync(isLoggedIn), catchAsync(acceptFollowRequest));
+router.route('/rejectfrequest').post(catchAsync(isLoggedIn), catchAsync(rejectFollowRequest));
 
 module.exports = router;
